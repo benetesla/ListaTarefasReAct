04c4cc73b73aa54d49d5e2a90f8ddde77a366e61
benetesla <78994881+benetesla@users.noreply.github.com>
2022-08-12 13:04:20 -0300

    alteracao footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>
-        <span>React + TS Todo</span> @ 2021
+        <span>React + TS Todo</span> @ 2022
       </p>
     </footer>
   );
